4edb3d5ef3ba57417a8a401dd8d1fc8dc0694053
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-26 19:35:32 -0700

    Create Problem2.cpp

---
+++ Project1/Problem2.cpp
@@ -0,0 +1,40 @@
+//Nhan Vo And Ian Lee
+// CECS 282 LAB 1-Problem 2
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+/*
+bin2Dec function:
+Take a reference of a string as parameter
+if the char in the string is not 0, add to sum 
+implement the power.
+*/
+int bin2Dec(const string& binaryString){
+    int power=1,sum=0;
+    for(int i=binaryString.length()-1;i>-1;i--){
+        if(binaryString[i]!='0'){
+            sum+=power;
+        }
+        power*=2;
+    }
+    return sum;
+    
+}
+/*
+Driver:
+Take string input from the user and pass it as a parameter to bin2Dec function
+output the result.
+*/
+int main()
+{
+    string input;
+    cout<<"Please enter your binary number"<<endl;
+    cin>>input;
+    cout<<"Binary Conversion: "<<bin2Dec(input)<<endl;
+    return 0;
+}
+
+
+
+
+
