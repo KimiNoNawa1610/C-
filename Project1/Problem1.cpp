c70e28955409e48594284b3730e7b7797a799493
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-31 14:08:20 -0700

    Update Problem1.cpp

--- Project1/Problem1.cpp
+++ Project1/Problem1.cpp
@@ -1,6 +1,5 @@
 //Nhan Vo
 // CECS 282 LAB 1-Problem 1
-#include <stdio.h>
 #include <iostream>
 using namespace std;
 /*
@@ -9,29 +8,50 @@ Find the remainder of the number and reduce it by 10 percent each itteration
 If the remainder is odd, add to the sum
 */
 int OddCalculation(int x){
-    int temp=0,sum=0;
+    
+    int temp=0;
+    int sum=0;
+    
     while(x>0){
-        temp=x%10;
-        x=x/10;
+        
+        temp=x%10;// seperate each number in x
+        
+        x=x/10;// remove the seperated number
+        
         if(temp%2!=0){
-            sum+=temp;
+            
+            sum+=temp;// If the seperated number is odd, add to sum
+            
         }
+        
     }
+    
     return sum;
+    
 }
 
 /*
-driver:
+Driver:
 Take the input from the user
 Call the OddCalculation() function with the number from the user.
 */
 int main()
 {
-    int input;
+    int input;// initialize user's input holder
+    
     cout<<"Please Enter your number"<<endl;
-    cin>>input;
-    cout<<OddCalculation(input);
+    
+    cin>>input;// store input
+    
+    cout<<OddCalculation(input);//call OddCalculation function
+    
     return 0;
+    
 }
+
+
+
+
+
 
 
