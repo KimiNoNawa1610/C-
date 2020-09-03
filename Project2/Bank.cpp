000f234bf7d61a47a56ecd6ddb9ddfc1eeb0e718
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-09-03 09:43:34 -0700

    Update Bank.cpp

--- Project2/Bank.cpp
+++ Project2/Bank.cpp
@@ -5,63 +5,81 @@
 #include"Account.h"
 #include<iostream>
 using namespace std;
-
+/*
+* Class Bank:
+* Has two objects, checking and savings of type Account
+*/
 class Bank {
 private:
 	Account checking;
 	Account savings;
 public:
-	Bank();
-	void deposit(double amount, string account);
-	void withdraw(double amount, string account);
-	void transfer(double amount, string account);
-	void print_balances();
+	/*Create function holder of class bank*/
+	Bank();// initialize the tempty bank accounts
+	Bank(double checking_amount, double savings_amount);// initialize bank accounts with certain amount of money
+	void deposit(double amount, string account);//Deposit into bank account(s)
+	void withdraw(double amount, string account);//Withdraw from bank account(s)
+	void transfer(double amount, string account);//Transfer from one account to another
+	void print_balances();// Print out the accounts' balance
 };
-
+// initialize the tempty bank accounts
 Bank::Bank() {
-	checking.get_balance();
-	savings.get_balance();
-
+	checking.deposit(0);// deposit amount is 0
+	savings.deposit(0);// deposit amount is 0
 }
-
+// initialize bank accounts with certain amount of money
+Bank::Bank(double checking_amount, double savings_amount) {
+	checking.deposit(checking_amount);// deposit amount is checking_amount
+	savings.deposit(savings_amount);// deposit amount is savings_amount
+}
+//Deposit into bank account(s)
 void Bank::deposit(double amount, string account) {
-	if (account == "S") {
+	//Identify account type that users want to deposit
+	if (account == "S") {// deposit to savings account
 		savings.deposit(amount);
 	}
-	else if (account == "C") {
+	else if (account == "C") {// deposit to checking account
 		checking.deposit(amount);
 	}
 }
-
+//Withdraw from bank account(s)
+/*Widthdraw function implement the withdraw function from Account class. 
+* If the account does not have more than the amount users want to withdraw, minus $5 from Account balance
+*/
 void Bank::withdraw(double amount, string account) {
-	if (account == "S") {
+	//Identify account type that users want to withdraw
+	if (account == "S") {// withdraw from savings account
 		savings.withdraw(amount);
 	}
-	else if (account == "C") {
+	else if (account == "C") {//withdraw from checking account
 		checking.withdraw(amount);
 	}
 }
-
+//Transfer from one account to another
 void Bank::transfer(double amount, string account) {
-	if (account == "S") {
+	//Identify account type that users want to transfer from
+	if (account == "S") {//Transfer from savings account
 		savings.withdraw(amount);
-		if (amount < savings.get_balance()) {
+		if (amount < savings.get_balance()) {// If the amount is no more than account balance then deposit
 			checking.deposit(amount);
 		}
 	}
-	else if (account == "C") {
+	else if (account == "C") {//Transfer from checking account
 		checking.withdraw(amount);
-		if (amount < checking.get_balance()) {
+		if (amount < checking.get_balance()) {// If the amount is no more than account balance then deposit
 			savings.deposit(amount);
 		}
 	}
 }
-
+// Print out the accounts' balance
 void Bank::print_balances() {
-	cout << "Your Checking Balance: $" << checking.get_balance()<<endl;
-	cout << "Your Savings Balance: $" << savings.get_balance() << endl;
+	cout << "Your Checking Balance: $" << checking.get_balance()<<endl;// output checking account balance
+	cout << "Your Savings Balance: $" << savings.get_balance() << endl;// output savings account balance
 }
-
+/*
+* Driver:
+* Create a Bank account and test every functions of class Bank
+*/
 int main() {
 	Bank my_bank;
 	cout << "Inital bank balances: \n";
@@ -79,11 +97,11 @@ int main() {
 	my_bank.print_balances();
 
 	cout << "Trying to transfer $900 from Checking.\n";
-	my_bank.transfer(900, "C");
+	my_bank.transfer(900, "C");//Should - 5 from acocunt and not transfer
 	my_bank.print_balances();
 
 	cout << "trying to transfer $900 from Savings.\n";
-	my_bank.transfer(900, "S");
+	my_bank.transfer(900, "S");//Should - 5 from acocunt and not transfer
 	my_bank.print_balances();
 	return 0;
 }
