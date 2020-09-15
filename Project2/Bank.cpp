c7bc84ac299f0e2b824e9d29ff84d54f8d869742
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-09-15 09:51:10 -0700

    Update Bank.cpp

--- Project2/Bank.cpp
+++ Project2/Bank.cpp
@@ -4,24 +4,9 @@
 */
 #include"Account.h"
 #include<iostream>
+#include"Bank.h"
 using namespace std;
-/*
-* Class Bank:
-* Has two objects, checking and savings of type Account
-*/
-class Bank {
-private:
-	Account checking;
-	Account savings;
-public:
-	/*Create function holder of class bank*/
-	Bank();// initialize the tempty bank accounts
-	Bank(double checking_amount, double savings_amount);// initialize bank accounts with certain amount of money
-	void deposit(double amount, string account);//Deposit into bank account(s)
-	void withdraw(double amount, string account);//Withdraw from bank account(s)
-	void transfer(double amount, string account);//Transfer from one account to another
-	void print_balances();// Print out the accounts' balance
-};
+
 // initialize the tempty bank accounts
 Bank::Bank() {
 	checking.deposit(0);// deposit amount is 0
@@ -75,33 +60,4 @@ void Bank::transfer(double amount, string account) {
 void Bank::print_balances() {
 	cout << "Your Checking Balance: $" << checking.get_balance()<<endl;// output checking account balance
 	cout << "Your Savings Balance: $" << savings.get_balance() << endl;// output savings account balance
-}
-/*
-* Driver:
-* Create a Bank account and test every functions of class Bank
-*/
-int main() {
-	Bank my_bank;
-	cout << "Inital bank balances: \n";
-	my_bank.print_balances(); /* set up empty accounts */
-
-	cout << "Adding some money to accounts: \n";
-	my_bank.deposit(1000, "S"); /* deposit $1000 to savings */
-	my_bank.deposit(2000, "C"); /* deposit $2000 to checking */
-	my_bank.print_balances();
-
-	cout << "Taking out $1500 from checking,and moving $200 from";
-	cout << " savings to checking.\n";
-	my_bank.withdraw(1500, "C"); /* withdraw $1500 from checking */
-	my_bank.transfer(200, "S"); /* transfer $200 from savings */
-	my_bank.print_balances();
-
-	cout << "Trying to transfer $900 from Checking.\n";
-	my_bank.transfer(900, "C");//Should - 5 from acocunt and not transfer
-	my_bank.print_balances();
-
-	cout << "trying to transfer $900 from Savings.\n";
-	my_bank.transfer(900, "S");//Should - 5 from acocunt and not transfer
-	my_bank.print_balances();
-	return 0;
 }
