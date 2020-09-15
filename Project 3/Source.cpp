326fdbdfde4412fa4119d0d6f9530cd4036fad4a
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-09-15 09:51:09 -0700

    Update Source.cpp

--- Project 3/Source.cpp
+++ Project 3/Source.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 #include<fstream>
+/*
+* Nhan Vo
+* CECS 282- Project 3
+*/
 using namespace std;
 // Reads data from a text file into an array
 int readData(int*& arr) {
@@ -20,86 +24,118 @@ int readData(int*& arr) {
     return size;
 } // End readData
 
+/*
+* arr is a pointer to an array of integers to be sorted. last is the number of elements in the array. 
+*/
 void bsort(int* arr, int last) {
-    bool isSorted = 0;
+    bool isSorted = false;// default that the list is not sorted
     // Sort the numbers using pointers 
     while (!isSorted) {
-        isSorted = 1;
-        for (int i = 0; i < last; i++) {
+        isSorted = true;// change the isSorted value to true
+        for (int i = 0; i < last; i++) {// create net loop to check 2 element in the array
             for (int j = i + 1; j < last; j++) {
-                if (*(arr + i) > * (arr + j)) {
+                if (*(arr + i) > * (arr + j)) {//The function bsort sorts the list of integers in ascending order.
+                    //Swap the i and j element in the array
                     int temp = *(arr + i);
                     *(arr + i) = *(arr + j );
                     *(arr + j) = temp;
-                    isSorted = 0;
+                    isSorted = false; // update the isSorted condition to false
                 }
             }
         }
     }
 }
 
+/*
+* arr is a pointer to an array of integers. last is the number of elements in the array.
+*/
 void writeToConsole(int* arr, int last) {
-    bsort(arr, last);
-    for (int i = 0; i < last; i++) {
+    for (int i = 0; i < last; i++) {// displays the list
         cout << *(arr + i) << ",";
     }
+    cout << endl;
 }
 
+/*
+* isGreater than function to compare if number a greater than number b
+*/
 bool isGreater(int a, int b) {
-    if (a > b) {
+    if (a > b) {// if a>b return true
         return true;
     }
-    return false;
+    return false;// else return false
 }
 
+/*
+* isLess than function to compare if number a less than number b
+*/
 bool isLess(int a, int b) {
-    if (a < b) {
+    if (a < b) {// if a<b return true
         return true;
     }
-    return false;
+    return false;// else return false
 }
 
+/*
+Pre condition
+array - a pointer to a an array of size element.
+pointer to function - a pointer to a function that compares two values (depending on sorting in ascending order or descending order)
+Post condition
+Sort the array in ascending or descending based on the the pointer to a function.
+*/
 void bubble_sort(int* array, int size, bool(*hold)(int, int)) {
-    bool isSorted = 0;
+    bool isSorted = false;
+    // default that the list is not sorted
+    // Sort the numbers using pointers 
+    //Similar to the bsort function
+    //except the condition value of bool (*hold)
     while (!isSorted) {
-        isSorted = 1;
-        for (int i = 0; i < size; i++) {
+        isSorted = true;// change the isSorted value to true
+        for (int i = 0; i < size; i++) {// create net loop to check 2 element in the array
             for (int j = i + 1; j < size; j++) {
-                bool cond = (*hold)(*(array + i), *(array + j));
-                if (cond==true) {
+                bool cond = (*hold)(*(array + i), *(array + j));// store the boolean value of ascending or descending condition 
+                if (cond==true) {//The function bsort sorts the list of integers in order according to the cond condition.
+                    //Swap the i and j element in the array
                     int temp = *(array + i);
                     *(array + i) = *(array + j);
                     *(array + j) = temp;
-                    isSorted = 0;
+                    isSorted = false;// update the isSorted condition to false
                 }
             }
         }
     }
 }
 
+/*
+* Driver:
+* Test the previous functions
+*/
+
 int main() {
 
-    int* arr;
+    int* arr;// create pointer of an integer
 
-    int n;
+    int n;// create an integer value
 
-    n = readData(arr);
+    cout <<"The size of the array (readData function tester)"<<endl;
+    n = readData(arr);// call readData to store the size of the array in the n value 
+    cout << "The array has size of: " << n << endl;
+    cout << endl;
+    cout << "bsort function testing:" << endl;
+    bsort(arr, n);// Test the bsort function by calling bsort to sort arr in ascending order
+    writeToConsole(arr, n);
 
-    bubble_sort(arr, n, isLess);
-    //print the sorted array
-    cout << "Array sorted in ascending order" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << *(arr + i) << " ";
-    }
+    cout << endl;
+    cout << "Array sorted in decending order:" << endl;
+    bubble_sort(arr, n, isLess);  // Test bubble_sort in ascending order
+    writeToConsole(arr, n);
+
     cout << endl;
 
-    cout << "Array sorted in decending order" << endl;
-    //call bubble_sort to sort the array
-    //pass compare_desc pointer to sort the array in decending order
-    bubble_sort(arr, n, isGreater);
-    for (int i = 0; i < n; i++) {
-        cout << *(arr + i) << " ";
-    }
+    cout << "Array sorted in ascending order:" << endl;
+    bubble_sort(arr, n, isGreater);// Test bubble_sort in deceding order
+    writeToConsole(arr, n);
+    
 
     return 0;
 }
